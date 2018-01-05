ba3f31fe4f26d42c8a3700adad6c9ea2186ab7de
Kelley Henican <kelleyhenican2@gmail.com>
2018-01-04 23:43:39 -0600

    fixed app.js

--- app.js
+++ app.js
@@ -2,30 +2,57 @@ function onReady() {
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
-
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
     // get the text
     let title = newToDoText.value;
-    // create a new Li
-    let newLi = document.createElement('li');
-    // create a new input
-    let checkbox = document.createElement('input');
-    // set the input's type to checkbox
-    checkbox.type = "checkbox";
-    // set the title
+    // create a new li
+   let newLi = document.createElement('li');
+   //add attribute id to newLi
+   //use time stamp for id data
+   // date.now()
+   let timeStamp = Date.now();
+   newLi.setAttribute('id',timeStamp);
+   // create a new input
+   let checkbox = document.createElement('input');
+
+   //create delete button
+  let button = document.createElement('button');
+
+  // modify the code so that the delete function is tied to id
+
+   // set the input's type to checkbox
+   checkbox.type = "checkbox";
+   // set the title
     newLi.textContent = title;
+
+    // set delete button text
+
+   button.textContent = "delete";
+
     // attach the checkbox to the li
     newLi.appendChild(checkbox);
-    //attach the li to the ul
+
+    //attach the delete button to the li
+    newLi.appendChild(button);
+
+    // attach the li to the ul
     toDoList.appendChild(newLi);
+
+
+    button.addEventListener("click", event => {
+      // change this line
+      let elem = document.getElementById(timeStamp);
+      elem.remove();
+    });
 
     //empty the input
     newToDoText.value = '';
-  });
+
+
+});
 
 }
-
 
 
 window.onload = function() {
